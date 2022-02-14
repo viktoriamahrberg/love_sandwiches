516d6bbed080eb139d376992b3b5d276c8a28576
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-14 09:24:58 +0000

    Create get_sales_data function to get data string from user input

--- run.py
+++ run.py
@@ -12,7 +12,33 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
-sales = SHEET.worksheet('sales')
+def get_sales_data():
+    """
+    Collects sales figures input from the user
+    """
 
-data = sales.get_all_values()
-print(data)
+    print('Please enter sales data from the last market')
+    print('Data should be six numbers, separated by commas')
+    print('Example: 10,20,30,40,50\n')
+
+    data_str = input("Enter your data here: ")
+    sales_data = data_str.split(",")
+    validate_data(sales_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+get_sales_data()
