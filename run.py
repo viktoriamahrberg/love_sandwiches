1b437e35065098829b7f6141f8f32e7673bf93df
Vikmah <viktoria.mahrberg@gmail.com>
2022-02-15 07:45:53 +0000

    Refactoring code for update worksheets

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
+
 def get_sales_data():
     """
     Get sales figures input from the user.
@@ -21,18 +22,20 @@ def get_sales_data():
     by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print('Please enter sales data from the last market')
-        print('Data should be six numbers, separated by commas')
-        print('Example: 10,20,30,40,50\n')
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, separated by commas.")
+        print("Example: 10,20,30,40,50,60\n")
 
         data_str = input("Enter your data here: ")
+
         sales_data = data_str.split(",")
-      
 
         if validate_data(sales_data):
-            print('Data is valid!')
+            print("Data is valid!")
             break
+
     return sales_data
+
 
 def validate_data(values):
     """
@@ -52,16 +55,18 @@ def validate_data(values):
 
     return True
 
-def update_sales_worksheet(data):
-    """
-    Update sales worksheet, add new row with data
-    """
-    print("Updating sales worksheet...\n")
-    sales_worksheet = SHEET.worksheet('sales')
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
 
-def calculate_surpus_data(sales_row):
+# def update_sales_worksheet(data):
+#    """
+#   Update sales worksheet, add new row with the list data provided
+#   """
+#   print("Updating sales worksheet...\n")
+#    sales_worksheet = SHEET.worksheet("sales")
+#   sales_worksheet.append_row(data)
+#   print("Sales worksheet updated successfully.\n")
+
+
+def calculate_surplus_data(data):
     """
     Compare sales with stock and calculate the surplus for each item type.
     The surplus is defined as the sales figure subtracted from the stock:
@@ -69,18 +74,46 @@ def calculate_surpus_data(sales_row):
     - Negative surplus indicates extra made when stock was sold out.
     """
     print("Calculating surplus data...\n")
-    stock = SHEET.worksheet('stock').get_all_values()
+    stock = SHEET.worksheet("stock").get_all_values()
     stock_row = stock[-1]
-    print(stock_row)
+    
+    surplus_data = []
+    for stock, sales in zip(stock_row, sales_row):
+        surplus = int(stock) - sales
+        surplus_data.append(surplus)
+
+    return surplus_data
+
+#def update_surplus_worksheet(surplus_data):
+#    """
+#    Update surplus worksheet, add new row with the list data provided
+#    """
+#    print("Updating surplus worksheet...\n")
+#   surplus_worksheet = SHEET.worksheet("surplus")
+#   surplus_worksheet.append_row(surplus_data)
+#   print("Surplus worksheet updated successfully.\n")
+
+def update_worksheet(data, worksheet):
+    """
+    Receives a list of integers to be inserted into a worksheet
+    Update the relevant worksheet with the data provided
+    """
+    print(f"Updating {worksheet} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet.to_update.append_row(surplus_data)
+    print(f"{worksheet} updated successfully.\n")
+
 
 def main():
-    """ 
+    """
     Run all program functions
     """
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_worksheet(sales_data)
-    calculate_surpus_data(sales_data)
+    update_worksheet(sales_data, "sales")
+    new_surplus_data = calculate_surplus_data(sales_data)
+    update_worksheet(new_surplus_data, "surplus")
 
-print("Welcome to Love Sandwiches data automation!")
+
+print("Welcome to Love Sandwiches Data Automation")
 main()
